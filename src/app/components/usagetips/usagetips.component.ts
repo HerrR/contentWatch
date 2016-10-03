f18bf4b16001988d3ec52d679306060be5bc13bb
samwah <samuel.wahlstedt@gmail.com>
2016-10-03 15:56:50 +0200

    Usage tip service started

--- src/app/components/usagetips/usagetips.component.ts
+++ src/app/components/usagetips/usagetips.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 // import { RouterLink } from '@angular/core';
+import { ContentService } from '../../services/content-service.service';
+import { QueryParams } from '../../dataTypes/queryParams';
+import { Subscription }   from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-usagetips',
   templateUrl: './usagetips.component.html'
 })
 export class UsagetipsComponent implements OnInit {
+  mostRecentQuery: QueryParams;
 
-  constructor() { }
-
+  constructor(private contentService:ContentService) {
+    contentService.queryParams$.subscribe(
+      data => {
+        this.mostRecentQuery = data;
+        this.onSearchEvent(data);
+      }
+    );
+  }
+  onSearchEvent(queryParams){
+    this.contentService.getUsageTips(queryParams)
+      .subscribe(
+        (data: any) => {
+          console.log(data)
+        }
+      );
+  }
   ngOnInit() {
   }
 
--- src/app/services/content-service.service.ts
+++ src/app/services/content-service.service.ts
@@ -24,12 +24,12 @@ export class ContentService {
     this.queryParams.next(qp);
   }
 
-  
+  /*
   buildEngineURL(env:Object){
     //let optionalDash = (env == "dev") ? "-" : "";
     //return `http://1-0-content${ optionalDash }engine-mi-${ env }-ebuilder.eu-west-1.elasticbeanstalk.com/v2/contentengine/`;
   }
-
+*/
   getNavigation(params): Observable<navEntries[]> {
       const headers = new Headers();
       //headers.append('x-guid', 'telia'+'-'+'test'+'#eu-west-1:8138c478-b446-4566-b7e1-b4507c05ecf1');
@@ -46,11 +46,23 @@ export class ContentService {
                   .map((r: Response) => r.json().data as navEntries[]);
   }
 
+  getUsageTips(params): Observable<string[]> {
+      const headers = new Headers();
+      headers.append('x-guid', params.env['x-guid']);
+      headers.append('x-channel-name', 'contentWatch');
+      //category is collection in usagetips
+      let queryString:string = `tips?query={"tags":{"tenant":["${params.tenant}"],"lang":["${params.lang}"],"category":["${params.category}"],"model":["${params.model}"]},"collection": ["battery", "camera", "hotapps", "lifehacks", "news", "startup"],"params":{"page":0}}`;
+      console.log(params.env['ce']+'v2/contentengine/'+queryString);
+      return this.http
+                  .get(params.env['ce']+'v2/contentengine/'+queryString, {headers: headers})
+                  .map((r: Response) => r.json().data as string[]);
+  }
+
   getSolutions(problemID: string, previousQueryParams: QueryParams): Observable<solutionData>{
     let queryType:string = "solutions/";
     const headers = new Headers();
     headers.append('x-guid', 'halebop#eu-west-1:8138c478-b446-4566-b7e1-b4507c05ecf1');
-    headers.append('x-channel-name', 'halebop-Android');
+    headers.append('x-channel-name', 'contentWatch');
   
     return this.http
                 .get(previousQueryParams.env['ce']+'v2/contentengine/'+queryType+problemID, {headers: headers})
