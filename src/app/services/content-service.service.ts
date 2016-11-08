cb8f0d6083be84ec040a94a2d92f3679594b159e
samwah <samuel.wahlstedt@gmail.com>
2016-11-08 16:09:54 +0100

    Cleanup in service

--- src/app/services/content-service.service.ts
+++ src/app/services/content-service.service.ts
@@ -7,7 +7,6 @@ import { solutionData } from '../dataTypes/solutionData';
 import { QueryParams } from '../dataTypes/queryParams';
 import { Subject }    from 'rxjs/Subject';
 
-
 @Injectable()
 export class ContentService {
 
@@ -15,29 +14,17 @@ export class ContentService {
     private http: Http
   ) {}
 
-  
   private queryParams = new Subject<QueryParams>();
-  
   queryParams$ = this.queryParams.asObservable();
   
   setQueryParams(qp: QueryParams){
     this.queryParams.next(qp);
   }
 
-  /*
-  buildEngineURL(env:Object){
-    //let optionalDash = (env == "dev") ? "-" : "";
-    //return `http://1-0-content${ optionalDash }engine-mi-${ env }-ebuilder.eu-west-1.elasticbeanstalk.com/v2/contentengine/`;
-  }
-*/
   getNavigation(params): Observable<navEntries[]> {
       const headers = new Headers();
-      //headers.append('x-guid', 'telia'+'-'+'test'+'#eu-west-1:8138c478-b446-4566-b7e1-b4507c05ecf1');
       headers.append('x-guid', params.env['x-guid']);
       headers.append('x-channel-name', 'contentWatch');
-                      /*,
-                      'X-Requested-With': 'com.teliasonera.deviceselfservice.telia',
-                      'x-api-key': 'Nqd1RbJkW1hoAlPu9xTcP2Vd5Ceg5AIy',*/
 
       let queryString:string = `navigation?query={"tags":{"tenant":["${params.tenant}"],"lang":["${params.lang}"],"category":["${params.category}"],"model":["${params.model}"],"os":["${params.os}"]},"params":{"page":0}}`;
       
@@ -66,13 +53,10 @@ export class ContentService {
   
     return this.http
                 .get(previousQueryParams.env['ce']+'v2/contentengine/'+queryType+problemID, {headers: headers})
-                //.get(this.buildEngineURL(previousQueryParams.env)+queryType+problemID, {headers: headers})
                 .map((r: Response) => r.json().data as solutionData);
   }
 
   getSolutionHTML(uri: string): Observable<string>{
-    //let baseURL: string = "https://s3-eu-west-1.amazonaws.com/content-int-test.ebuilder.io";
-
     return this.http
                 .get(uri)
                 .map((r: Response) => r.text() as string);
