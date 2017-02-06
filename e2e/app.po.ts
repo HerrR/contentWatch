66c51006fff8a4f9bd46a72ff5a10ed198835694
samwah <samuel.wahlstedt@gmail.com>
2017-02-06 15:05:28 +0100

    Update process started
    updating to 2.3.1 and to @angular/cli

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor/globals';
+import { browser, element, by } from 'protractor';
 
-export class ContentWatch2Page {
+export class ContentWatchPage {
   navigateTo() {
     return browser.get('/');
   }
